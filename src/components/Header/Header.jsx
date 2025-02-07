2d2484784a5cff832ebb2e82fe45d7e19d2405ba
Milton Chung <miltonjchung@gmail.com>
2025-02-06 22:26:28 -0800

    exercise 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,23 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopHeader>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopHeader>
         <Logo />
+        <DesktopHeader>
+          <CallToActionGroup>
+            <Button>Subscribe</Button>
+            <Link href="#">Already a subscriber?</Link>
+          </CallToActionGroup>
+        </DesktopHeader>
       </MainHeader>
     </header>
   );
@@ -39,6 +55,18 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: revert;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +87,33 @@ const ActionGroup = styled.div`
   }
 `;
 
+const CallToActionGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-self: flex-end;
+  gap: 8px;
+`;
+
+const Link = styled.a`
+  color: ${COLORS.gray[900]};
+  text-decoration: underline;
+  font-style: italic;
+  font-size: 14px;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
